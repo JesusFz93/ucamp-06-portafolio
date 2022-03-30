f2508fbe092e1397a558e63170318b82bcc6fd81
Jesus <jesus.fdz93@gmail.com>
2022-03-30 11:23:15 -0500

    Proyecto inicializado

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const AboutPage = () => {
+  return (
+    <h1>
+      <em>C</em>
+      <em className="planet left">O</em>
+      <em>S</em>
+      <em>M</em>
+      <em className="planet right">O</em>
+      <em>S</em>
+    </h1>
+  );
+};
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const HomePage = () => {
+  return (
+    <>
+      <main
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "90vh" }}
+      >
+        <h1 className="fw-bold " style={{ fontSize: "100px" }}>
+          Portfolio
+        </h1>
+      </main>
+    </>
+  );
+};
